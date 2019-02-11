b6d115ac9279e1761d8d433ed6ea95bfa2393c8f
Eric Florence <ericflorence.dev@gmail.com>
2019-02-11 15:19:23 -0800

    Update the navigation bar - Add login, signup, logout and edit profile links on the navigation bar- Create responsive directory inside the stylesheets directory and add CSS. - Add CSS to tweak navigation bar style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, :controllers => {:registration => "users/registrations#new"}
   root :to => 'pages#index'
 
   devise_scope :user do
